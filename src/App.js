d6d331abd6c2af2971d9ee50536e4e2c0bd5710f
maayeesha <maayeesha@gmail.com>
2021-10-09 01:08:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  TwitterAuthProvider,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import "./App.css";
+import initializeAuthentication from "./Firebase/firebase.initialize";
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
+const twitterProvider = new TwitterAuthProvider();
 
 function App() {
+  const auth = getAuth();
+  const [user, setUser] = useState({});
+  const handleGoogleSignin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  const handleTwitterSignin = () => {
+    signInWithPopup(auth, twitterProvider).then((result) => {
+      const { displayName, email, photoURL } = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        photo: photoURL,
+      };
+      setUser(loggedInUser);
+    });
+  };
+
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      setUser({});
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!user.name ? (
+        <div className="p-5">
+          <Button variant="danger" className="m-3" onClick={handleGoogleSignin}>
+            {" "}
+            Sign In with Google{" "}
+          </Button>
+          <Button variant="info" onClick={handleTwitterSignin}>
+            {" "}
+            Sign In with Twitter{" "}
+          </Button>
+        </div>
+      ) : (
+        <Button variant="seondary" onClick={handleSignOut}>
+          {" "}
+          Sign Out{" "}
+        </Button>
+      )}
+
+      <br />
+
+      {user.email && (
+        <div>
+          <h1>User Info</h1>
+          <h2>Welcome {user.name}</h2>
+          <p>Email address: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABk7VUqalGhoOJDk67uJBbRKFNI2wdIrA",
+  authDomain: "auth-log-in.firebaseapp.com",
+  projectId: "auth-log-in",
+  storageBucket: "auth-log-in.appspot.com",
+  messagingSenderId: "403210373407",
+  appId: "1:403210373407:web:4bc84da6d4e46a4d6d882a",
+};
+
+export default firebaseConfig;
